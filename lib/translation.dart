9d6a6db58491fbf492875c3cc6e88520a8c01abc
officialbishowb <officialbishowb@gmail.com>
2023-05-11 15:24:42 +0200

    optimized code - only 1 map which con. everything

--- lib/translation.dart
+++ lib/translation.dart
@@ -40,65 +40,55 @@ void getTranslationFolder() async {
   //   ])
   // ]);
 
-  Map<int, String> mainFolders = HashMap();
-  Map<int, Map<String, String>> subFolders = HashMap();
-  Map<String, List<String>> subFoldersMainContent = HashMap();
+  Map<int, Map<String, dynamic>> folders = {};
 
-  // Read the main folder headers. The format is: 0, Section
   var mainHeader = await helper
       .getLocaleTranslationFileContent("docs_titles_$languageCode.csv");
   var mainHeaderLines = mainHeader.split("\n");
-  String mainLine;
-  for (mainLine in mainHeaderLines) {
-    List<String> lineSplit = mainLine.split(",");
-    mainFolders[int.parse(lineSplit[0])] = lineSplit[1];
 
-    // Read the sub folder headers. The format is: 00, 0, Header where 00 is the sub folder id and 0 is the main folder id
+  for (var mainLine in mainHeaderLines) {
+    var mainLineSplit = mainLine.split(",");
+    var mainFolderId = int.parse(mainLineSplit[0]);
+    var mainFolderTitle = mainLineSplit[1];
+
+    folders[mainFolderId] = {
+      "title": mainFolderTitle,
+      "subFolders": {},
+      "content": {},
+    };
+
     var subHeader = await helper
         .getLocaleTranslationFileContent("docs_headings_$languageCode.csv");
     var subHeaderLines = subHeader.split("\n");
-    String subLine;
-    for (subLine in subHeaderLines) {
-      List<String> subLineSplit = subLine.split(","); // 00, 0, Header
-      if (subLineSplit[1] == lineSplit[0]) {
-        if (subFolders[int.parse(subLineSplit[1])] == null) {
-          subFolders[int.parse(subLineSplit[1])] = HashMap();
-        }
 
-        subFolders[int.parse(subLineSplit[1])]![subLineSplit[0]] =
-            subLineSplit[2];
+    for (var subLine in subHeaderLines) {
+      var subLineSplit = subLine.split(",");
+      var subFolderId = subLineSplit[0];
+      var mainFolderIdFromSub = int.parse(subLineSplit[1]);
+
+      if (mainFolderIdFromSub != mainFolderId) continue;
+
+      folders[mainFolderId]!["subFolders"][subFolderId] = subLineSplit[2];
+    }
+
+    var subContent = await helper
+        .getLocaleTranslationFileContent("docs_texts_$languageCode.csv");
+    var subContentLines = subContent.split("\n");
+
+    for (var subContentLine in subContentLines) {
+      var subContentLineSplit = helper.customSplit(subContentLine, ",", 2);
+      var subFolderIdFromContent = subContentLineSplit[0];
+
+      if (folders[mainFolderId]!["content"][subFolderIdFromContent] == null) {
+        folders[mainFolderId]!["content"][subFolderIdFromContent] = [];
       }
 
-      // Read the sub folder actual content(text). The format is: 00, 0. The 0 can be increment as it can be multiple lines because of the max character limit
-      var subContent = await helper
-          .getLocaleTranslationFileContent("docs_texts_$languageCode.csv");
-      var subContentLines = subContent.split("\n");
-      String subContentLine;
-      int i = 0;
-      for (subContentLine in subContentLines) {
-        if (i == 5) break;
-        // Split only twice as the content can have commas
-        List<String> subContentLineSplit =
-            helper.customSplit(subContentLine, ",", 2);
+      if (subContentLineSplit.length < 3) continue;
 
-        if (subContentLineSplit.length < 3) continue;
-        if (subFoldersMainContent[subContentLineSplit[0]] == null) {
-          if (subContentLineSplit[2].isNotEmpty) {
-            subFoldersMainContent[subContentLineSplit[0]] = [
-              subContentLineSplit[2]
-            ];
-          } else {
-            subFoldersMainContent[subContentLineSplit[0]] = [];
-          }
-        } else if (subContentLineSplit[2].isNotEmpty) {
-          subFoldersMainContent[subContentLineSplit[0]]!
-              .add(subContentLineSplit[2]);
-        }
-
-        //
+      var content = subContentLineSplit[2].trim();
+      if (content.isNotEmpty) {
+        folders[mainFolderId]!["content"][subFolderIdFromContent]!.add(content);
       }
     }
   }
-
-  print(subFoldersMainContent);
 }
